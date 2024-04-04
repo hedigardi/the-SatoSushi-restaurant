932d8e67ace46561fd87356835536574ebbe2182
Hedi Gardi <hedigardi@hotmail.com>
2024-04-04 19:28:11 +0200

    added content to create booking page

---
+++ therestaurant/src/pages/CreateBooking.jsx
@@ -0,0 +1,10 @@
+const CreateBooking = () => {
+  return (
+    <div>
+      <h2>Create New Booking</h2>
+      {/* Placeholder för formulär för att skapa en ny bokning */}
+    </div>
+  );
+};
+
+export default CreateBooking;
