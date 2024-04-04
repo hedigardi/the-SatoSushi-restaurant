4491b86cb89bcf2874dcaec94cf2002fe7c5bc0e
Hedi Gardi <hedigardi@hotmail.com>
2024-04-04 14:18:22 +0200

    added contact information

---
+++ therestaurant/src/pages/ContactPage.jsx
@@ -0,0 +1,14 @@
+const ContactPage = () => {
+  return (
+    <div>
+      <section>
+        <h2>Contact Information</h2>
+        <p>Address: Sushigatan 1</p>
+        <p>Phone: +46 (0) 12 34 56 789</p>
+        <p>Email: info@satosushi.com</p>
+      </section>
+    </div>
+  );
+};
+
+export default ContactPage;
