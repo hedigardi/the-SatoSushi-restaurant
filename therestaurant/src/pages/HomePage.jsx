8ba01db131327efff7c449a26e6a8a521a3c96bc
Rowel Malmström <143719030+rowpacmal@users.noreply.github.com>
2024-04-10 16:43:14 +0200

    improved Header html Co-authored-by: Antonio <toni.lingardsson@gmail.com>

--- therestaurant/src/pages/HomePage.jsx
+++ therestaurant/src/pages/HomePage.jsx
@@ -1,38 +1,38 @@
 const HomePage = () => {
   return (
-    <div className="columns">
-      <div className="column">
-        <p className="subtitle">Sushi Restaurang sedan 2008</p>
-        <div className="card">
-          <div className="card-content">
-            <div className="content">
-              <div className="hero is-medium is-primary is-bold">
-                <div className="hero-body">
-                  <h2>Sushi</h2>
-                  <p className="about_us">
-                    Njut av den utsökta smaken av traditionell japansk mat på
-                    SatoSuShi. Våra mästerkockar tillagar konstfullt varje rätt
-                    till perfektion, och kombinerar färska ingredienser med
-                    hundraåriga kulinariska tekniker.
-                  </p>
-                  <p className="about_us">
-                    Fördjupa dig i de rika smakerna av sushi, sashimi och andra
-                    japanska delikatesser när du ger dig ut på en kulinarisk
-                    resa som ingen annan.
-                  </p>
-                  <p className="about_us">
-                    Upplev essensen av Japan här på SatoSuShi.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="content">
-              <p>
-                Välkommen till SatoSuShi, din främsta källa för allt som rör
-                sushi.
-              </p>
-            </div>
-          </div>
+    <div>
+      <p className="subtitle">Sushi Restaurang sedan 2008</p>
+
+      <div className="card">
+        <div className="hero">
+          <article>
+            <header>
+              <h1>Sushi</h1>
+            </header>
+
+            <p className="about-us">
+              Njut av den utsökta smaken av traditionell japansk mat på
+              SatoSuShi. Våra mästerkockar tillagar konstfullt varje rätt till
+              perfektion, och kombinerar färska ingredienser med hundraåriga
+              kulinariska tekniker.
+            </p>
+
+            <p className="about-us">
+              Fördjupa dig i de rika smakerna av sushi, sashimi och andra
+              japanska delikatesser när du ger dig ut på en kulinarisk resa som
+              ingen annan.
+            </p>
+
+            <p className="about-us">
+              Upplev essensen av Japan här på SatoSuShi.
+            </p>
+          </article>
+        </div>
+
+        <div>
+          <p>
+            Välkommen till SatoSuShi, din främsta källa för allt som rör sushi.
+          </p>
         </div>
       </div>
     </div>
